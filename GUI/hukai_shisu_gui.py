dd65e3ab82744ca984836a789f9863e62353d860
shinshin86 <beagles1986@gmail.com>
2015-09-28 22:55:27 +0900

    テンプレート新規追加。本日の不快指数を算出するための機能の骨組みを実装。

--- GUI/hukai_shisu_gui.py
+++ GUI/hukai_shisu_gui.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
-from bottle import route, run
+from bottle import route, run, template
 
 
-# 現在はURLから温度・湿度を取得
-# TODO - 別の取得方法を後々実装する
+# URLから温度・湿度を取得
 @route('/<temperature:int>/<humidity:int>')
 def print_hukaiShisu(temperature,humidity):
     hukai_shisu_result = 0.81 * float(temperature) + 0.01 * float(humidity) * (0.99 * float(temperature) - 14.3) + 46.3
-    print("run hukai_shisu_gui")
-    return "温度 : " + str(temperature) +  " 湿度 : " + str(humidity) + " 不快指数 : " + str(hukai_shisu_result)
+    
+    show_hukai_shisu = "温度 : " + str(temperature) +  " 湿度 : " + str(humidity) + " 不快指数 : " + str(hukai_shisu_result)
+
+    return show_hukai_shisu
+
+
+
+# 本日の不快指数を表示
+@route('/today')
+def today():
+    
+    # 本日の不快指数を取得、処理。
+    today_temperature = 20
+    today_humidity = 70
+    today_hukai_shisu_result = 0.81 * float(today_temperature) + 0.01 * float(today_humidity) * (0.99 * float(today_temperature) - 14.3) + 46.3
+    result_tempe = "温度 : " + str(today_temperature)
+    result_humi = " 湿度 : " + str(today_humidity)
+    result_hukai = " 本日の不快指数 : " + str(today_hukai_shisu_result)
+    
+    # テンプレート読み込み
+    return template('index', html_today_temperature=result_tempe, html_today_humidity=result_humi,html_today_hukai_shisu_result=result_hukai)
+
+
 
 # サーバーの設定(開発用) - debugとreloaderを有効にしている
 run(host='localhost', port=8081, debug=True, reloader=True)
---
+++ GUI/views/index.tpl
@@ -0,0 +1,3 @@
+<h1>{{html_today_temperature}}</h1>
+<h1>{{html_today_humidity}}</h1>
+<h1>{{html_today_hukai_shisu_result}}</h1>
