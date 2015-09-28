67e337cfc11fdaba03c42f1571ecde149b749e88
shinshin86 <beagles1986@gmail.com>
2015-09-28 21:26:37 +0900

    GUI版をPython3に対応

--- GUI/hukai_shisu_gui.py
+++ GUI/hukai_shisu_gui.py
@@ -7,7 +7,7 @@ from bottle import route, run
 @route('/<temperature:int>/<humidity:int>')
 def print_hukaiShisu(temperature,humidity):
     hukai_shisu_result = 0.81 * float(temperature) + 0.01 * float(humidity) * (0.99 * float(temperature) - 14.3) + 46.3
-    print "run hukai_shisu_gui"
+    print("run hukai_shisu_gui")
     return "温度 : " + str(temperature) +  " 湿度 : " + str(humidity) + " 不快指数 : " + str(hukai_shisu_result)
 
 # サーバーの設定(開発用) - debugとreloaderを有効にしている
